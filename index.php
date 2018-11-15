67fc484900161c74b8d5ef1d211cdce441364db5
Turkmen48 <45080586+Turkmen48@users.noreply.github.com>
2018-11-15 23:57:42 +0300

    Add files via upload
    Proje Dosyaları
    (Project Files)
    -Only Turkish,  but you should do change Turkish sentences-

---
+++ index.php
@@ -0,0 +1,36 @@
+<center>
+<img src="edebi.gif"></img>
+</center>
+
+<?php
+ $soz[1] = "Ne demiş şair; uyuyalım! Hiçbir şey olmazsa, sabah olur."; // Söz 1 
+ $soz[2] = "Kalp sevmekten yorulmaz, sevene sebep sorulmaz."; // Söz 2
+ $soz[3] = "Hayatınızın kalitesini, hayatınızdaki insanların kalitesi belirler. ~M. Longston~"; // Söz 3
+ $soz[4] = "Sesini değil sözünü yükselt! Yağmurlardır büyüten zambakları, gök gürültüleri değil."; // Söz 4
+ $soz[5] = "Her kahkahanda Allah’a teşekkür etmiyorsan, neden her ağladığında O’na kızıyorsun?"; // Söz 5
+ $soz[6] = "Ben senin; sevgilin, eşin, baban, ağabeyin, arkadaşınım. Biri bitse biri kalır, seni hiç bırakmayacağım. ~Cemal Süreya~"; // Söz 6
+ $soz[7] = "Sükût eyle ey kalbim, kalmadı artık söylenecek kelam. Aşktan anlamayana yar olacağıma, yalnız kalırım vesselam."; // Söz 7
+ $soz[8] = "Belki de konuşuyordur gözlerin ama ben gözce bilmiyorum ki; sessizce biliyorum, usulca biliyorum, masumca biliyorum. ~Cemal Süreya~"; // Söz 8
+ $soz[9] = "Aşk, bazen gitmekle kalmak arasında verdiğin en büyük savaştır. Sevmeyenin aklı, gerçekten sevenin kalbi kazanır bu savaşı. ~Nazım Hikmet~"; // Söz 9
+ $soz[10] = "Mahşerde en yakınımızı bile tanıyamayacağımız söylenir ya hep, benim gönül gözüm seni bir yerden ısıracak mutlaka. Adım gibi, adın gibi biliyorum bunu!"; // Söz 10 
+ $soz[11] = "Baharı yaz uğruna tükettik, aşkı naz uğruna. Ve papatyaları seviyor sevmiyor uğruna. Derken ömrü tükettik bir hiç uğruna. ~Sezai Karakoç~"; // Söz 11 
+ $soz[12] = "Herkesin bir umudu vardır. Bir savaşı, bir kaybedişi, bir acısı, bir yalnızlığı, bir hüznü. Çünkü herkesin bir gideni vardır, içinden bir türlü uğurlayamadığı. ~Turgut Uyar~"; // Söz 12
+ $soz[13] = "Gidersen şair olurum, kalırsan senin. ~İbrahim Kurt~"; // Söz 13
+ $soz[14] = "Bir soğan soyuluyor, yaşarıyor gözler. Bir de devlet soyuluyor, aldırmıyor öküzler. ~Şair Eşref~"; // Söz 14
+ $soz[15] = "Kalbimde kalbine yok bile kinim. Bence artık sende herkes gibisin. ~Nazım Hikmet~"; // Söz 15
+ $soz[16] = "Ne de güzel demiş şair; tutsak kaldığın yer sevdiğin kişinin kalbiyse özgürlüğün canı cehenneme!"; // Söz 16
+ $soz[17] = "Hiç dokunmadığın birine aşık olabiliyorsan, işte sen aşkı hak ediyorsun. ~Can Dündar~"; // Söz 17 
+ $soz[18] = "Aşk diye bir şey yok ki. Sadece biraz heyecan duyuyoruz bunu da kendimize aşk diye yediriyoruz. ~Gore Vidal~"; // Söz 18
+ $soz[19] = "Üzülmüyorum. Çünkü hayatımdan çıkan hiç kimsenin hayatımda yeri yok. Düşünüyorum da değersizlere bu satırlar bile çok. Yılmaz Erdoğan"; // Söz 19
+ $karistir = rand(1,19);
+ echo "$soz[$karistir]"; // Ekrana basalım.
+?>
+
+ 
+ 
+ 
+ 
+ 
+<center>
+<img src="edebi.gif"></img>
+</center>
